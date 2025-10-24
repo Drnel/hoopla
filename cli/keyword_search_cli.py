eecda2e81853460630fc17d3a5f533d1f6662049
Drnel <mahesh121995@gmail.com>
2025-10-24 10:29:39 +0000

    added basic keyword search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 
+
+def file_search(query): 
+    with open("data/movies.json", "r") as file:
+        data = json.load(file)
+    movie_list = []
+    for movie in data["movies"]:
+        if query in movie["title"]:
+            movie_list.append(movie)
+    movie_list = movie_list[:5]
+    for i in range(len(movie_list)):
+        print(f"{i + 1}. {movie_list[i]["title"]}")
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -15,6 +27,7 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
+            file_search(args.query)
             pass
         case _:
             parser.print_help()
